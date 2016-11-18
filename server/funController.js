97b7d7fbe12799834f2f7f4272c892544fa29d27
Thai-Duong Nguyen <soleiluwedu@Not-an-NSA-Surveillance-Node.local>
2016-11-18 10:12:45 -0800

    Styles updated

--- server/funController.js
+++ server/funController.js
@@ -7,16 +7,24 @@ const factual = new Factual(`Jr4VU8j7IWGNP3P8tg2x21WVC58Opn0w7Zr5EUeo`, `rYkYbju
 const style = {
     reset: `\x1b[0m`,
     bold: `\x1b[1m`,
+    faint: `\x1b[2m`,
+    standout: `\x1b[3m`,
     underline: `\x1b[4m`,
+    blink: `\x1b[5m`,
     inverse: `\x1b[7m`,
+    hidden: `\x1b[8m`,
+    nostandout: `\x1b[23m`,
+    nounderline: `\x1b[24m`,
+    noblink: `\x1b[25m`,
+    noreverse: `\x1b[27m`,
     black: `\x1b[30m`,
-    white: `\x1b[37m`,
     red: `\x1b[31m`,
     green: `\x1b[32m`,
     yellow: `\x1b[33m`,
     blue: `\x1b[34m`,
     magenta: `\x1b[35m`,
-    cyan: `\x1b[36m`
+    cyan: `\x1b[36m`,
+    white: `\x1b[37m`
 }
 
 const funController = {
