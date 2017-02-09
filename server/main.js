564ffec3535a238b78cf8626c6ce400865a97232
Thai-Duong Nguyen <soleiluwedu@Not-a-CIA-Watch-Node.local>
2017-02-09 01:13:31 -0800

    Added comments to code in main.js

--- server/main.js
+++ server/main.js
@@ -4,18 +4,18 @@ class Itinerary {
 
 	constructor() {
 
-		// Creating pic
+		// Loading pic.
 		const pic = document.createElement(`img`);
 		pic.id = `pic`;
 		pic.src = `ferris.jpg`;
 		document.getElementById(`top`).appendChild(pic);
 
-		// Creating button div
+		// Creating button div.
 		const btns = document.createElement(`div`);
 		btns.id = `allbtns`;
 		document.getElementById(`nav`).appendChild(btns);
 
-		// Pointers to DOM elements that will be manipulated
+		// Pointers to DOM elements that will be manipulated.
 		this.main = document.getElementById(`main`);
 		this.lastBtnClicked = null;
 		this.allbtns = btns;
@@ -23,7 +23,7 @@ class Itinerary {
 	}
 
 	clearPlaces() {
-		// Clear out any Loading message or Places div
+		// Clear out any Loading message or Places div.
 		const loading = document.getElementById(`loading`);
 		if (loading) loading.remove();
 		const places = document.getElementById(`places`);
@@ -31,7 +31,7 @@ class Itinerary {
 	}
 
 	addLoadingMsg() {
-		// Add "Loading" message as a placeholder until data returns
+		// Add Loading message as a placeholder until data returns.
 		const loading = document.createElement(`p`);
 		loading.id = `loading`;
 		loading.textContent = `⏰ Downloading data...`;
@@ -45,47 +45,49 @@ class Itinerary {
 			btnCreated.className = `btn`;
 			btnCreated.id = btnpair.route;
 
-			// Button click listener
+			// Button click listener.
 			btnCreated.addEventListener(`click`, e => {
 
 				this.clearPlaces();
 
 				this.addLoadingMsg();
 
-				// Sending request to Factual.com API for data
+				// Sending request to Factual.com API for data.
 				const xhr = new XMLHttpRequest();
 				xhr.open(`GET`, `/${btnpair.route}`);
 				xhr.setRequestHeader("Content-type", "text/html");
 				xhr.onload = () => this.showPlaces(JSON.parse(xhr.responseText));
 				xhr.send();
 
-				// Changing button classes for highlighting purposes
+				// Changing button classes for highlighting purposes.
 				if (this.lastBtnClicked) this.lastBtnClicked.className = `btn`;
 				this.lastBtnClicked = btnCreated;
 				this.lastBtnClicked.className = `btn btnselected`;
 
-				// Not choosing to use GPS location due to small radius of Factual.com API search
+				// Not choosing to use GPS location due to small radius of Factual.com API search.
 				// console.log("Retrieving location from Google Maps API to send...");
 				// navigator.geolocation.getCurrentPosition(sendPosition, error);
 			})
-			// Append button to DOM
+			// Append button to DOM.
 			this.allbtns.appendChild(btnCreated);
 		});
 	}
 
 	showPlaces(objArr) {
 
+		// Clear any existing Loading message or Places div.
 		this.clearPlaces();
 
-		// Making new places div
+		// Making new Places div.
 		const places = document.createElement(`div`);
 		places.id = `places`;
 
+		// Creating div to show all suggested venues.
 		objArr.forEach(obj => {
 
 			const entry = document.createElement(`p`);
 
-			// Description/comment (not from API)
+			// Description of venue suggestion (not from API).
 			const desc = document.createElement(`span`);
 			desc.className = `desc`;
 			desc.textContent = obj.desc;
@@ -93,12 +95,12 @@ class Itinerary {
 
 			entry.innerHTML += `<br>`;
 
-			// Name of place
+			// Name of venue.
 			const name = document.createElement(`span`);
 			name.className = `name`;
 			name.textContent = obj.venue.name;
 			entry.appendChild(name);
-			// Neighborhood field is not always populated
+			// Neighborhood field is not always populated.
 			if (obj.venue.neighborhood) {
 				entry.innerHTML += ` in `;
 				const neighborhood = document.createElement(`span`);
@@ -109,14 +111,14 @@ class Itinerary {
 
 			entry.innerHTML += `<br>`;
 
-			// Building up address
+			// Building up address.
 			const address = document.createElement(`span`);
 			address.className = `address`;
-			// Some landmarks have no number address
+			// Some landmarks have no number address.
 			if (obj.venue.address) address.textContent += `${obj.venue.address}, `
 			address.textContent += `${obj.venue.locality}, ${obj.venue.region} ${obj.venue.postcode}`;
 
-			// Appending divs to DOM
+			// Appending Places div to DOM.
 			entry.appendChild(address);
 			places.appendChild(entry);
 			this.main.appendChild(places);
@@ -124,7 +126,10 @@ class Itinerary {
 	}
 }
 
+// Instantiating Itinerary class.
 const Ferris = new Itinerary();
+
+// Making buttons.
 Ferris.makeButtons([
 	{ route: `firstdate`, desc: `First Date` },
 	{ route: `seconddate`, desc: `Second Date` },
