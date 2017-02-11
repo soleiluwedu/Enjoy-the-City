3e715db66cdf9064ad88285f8abde4275d4a5995
Thai-Duong Nguyen <soleil@uw.edu>
2017-02-11 12:46:45 -0800

    cli updated with cleaner console log

--- server/cli.js
+++ server/cli.js
@@ -29,7 +29,7 @@ const cli = {
 		return next();
 	},
 	response: (req, res, next) => {
-		if (res.locals.err) console.log(`❗${style.bold}${style.red}${res.locals.err.message}${style.reset}`);
+		if (res.locals.err) console.log(`❗ ${style.bold}${style.red}${res.locals.err.message}${style.reset}`);
 		else console.log(`🌰 ${style.cyan}PAYLOAD delivered for GET request for ${style.red}${req.url}${style.reset}`);
 	}
 }
