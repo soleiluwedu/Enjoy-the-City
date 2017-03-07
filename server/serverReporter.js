7d6bfd1349de52baf517f1c5a2d9bd9b4a8c053b
Thai-Duong Nguyen <soleil@uw.edu>
2017-03-07 13:36:42 -0800

    256 colors added

--- server/serverReporter.js
+++ server/serverReporter.js
@@ -3,8 +3,8 @@
 // util used for logging objects on the request object.
 const util = require('util');
 
-// ansi style codes for the terminal.
-const ansi = {
+// ansi 16 color style codes for the terminal.
+const ansi16 = {
 	reset: '\x1b[0m',
 	bright: '\x1b[1m',
 	dim: '\x1b[2m',
@@ -36,9 +36,42 @@ const ansi = {
 	bg_white: '\x1b[47m'
 }
 
+// ansi 256 color style codes for the terminal.
+const ansi256 = {
+	reset: '\x1b[0m',
+	bright: '\x1b[1m',
+	dim: '\x1b[2m',
+	standout: '\x1b[3m',
+	underline: '\x1b[4m',
+	blink: '\x1b[5m',
+	inverse: '\x1b[7m',
+	hidden: '\x1b[8m',
+	nobright: '\x1b[22m',
+	nostandout: '\x1b[23m',
+	nounderline: '\x1b[24m',
+	noblink: '\x1b[25m',
+	noinverse: '\x1b[27m',
+	black: '\x1b[38;5;0m',
+	red: '\x1b[38;5;196m',
+	green: '\x1b[38;5;36m',
+	yellow: '\x1b[38;5;208m',
+	blue: '\x1b[38;5;39m',
+	magenta: '\x1b[38;5;129m',
+	cyan: '\x1b[38;5;39m',
+	white: '\x1b[38;5;255m',
+	bg_black: '\x1b[48;5;0m',
+	bg_red: '\x1b[48;5;196m',
+	bg_green: '\x1b[48;5;36m',
+	bg_yellow: '\x1b[48;5;208m',
+	bg_blue: '\x1b[48;5;39m',
+	bg_magenta: '\x1b[48;5;129m',
+	bg_cyan: '\x1b[48;5;39m',
+	bg_white: '\x1b[48;5;255m'
+}
+
 // Change this variable to point to another color code object
 // if your terminal uses a different color code scheme.
-var style = ansi;
+var style = ansi256;
 
 // Style associations.
 const msgDecor = {
